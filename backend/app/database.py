14e4ca7f6196468dd31e241a72d34ab40441a28c
Gokulkiran418 <umgokulkiran@gmail.com>
2025-06-19 18:49:04 +0530

    edit database_url

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,17 +1,15 @@
-
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Database URL (update with your PostgreSQL credentials)
-
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:gogeta2182@localhost:5432/splitwise"
+# Read DATABASE_URL from environment
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:mypassword@localhost:5432/splitwise")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-# Dependency to get DB session
 def get_db():
     db = SessionLocal()
     try:
